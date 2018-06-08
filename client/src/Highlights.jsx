9dbb2a979fb0b8c2257aa5479b4101c3c7c90f7b
Jonathan Pizzolato <jpizzolato36@gmail.com>
2018-06-08 15:16:14 -0700

    Created basic shell for top three home highlights.

--- client/src/Highlights.jsx
+++ client/src/Highlights.jsx
@@ -1,122 +1,235 @@
 import React from 'react';
+import Styled from 'styled-components';
 
-const Highlights = () => (
-  <div>
-  <div>
-    <div>
-      <div style="margin-bottom:24px">
-      <div class="_1d6n3ar">
-        <small class="_1wk8be92">
-          <span>HOME HIGHLIGHTS</span>
-        </small>
-      <div style="margin-top:12px">
+const Div_1d6n3ar = Styled.div`
+  border: 1px solid #DBDBDB !important;
+  border-radius: 4px !important;
+  box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.05) !important;
+  padding: 24px !important;
+  box-sizing: border-box;
+  display: block;
+`;
+const Div_5y40cyp = Styled.div`
+  margin: 0px !important;
+  word-wrap: break-word !important;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+  font-size: 14px !important;
+  line-height: 18px !important;
+  letter-spacing: normal !important;
+  padding-top: 0px !important;
+  padding-bottom: 0px !important;
+  color: #484848 !important;
+  font-weight: normal !important;
+  -webkit-font-smoothing: antialiased;
+  box-sixing: border-box;
+  display: block;
+`;
+const Div_10k87om = Styled.div`
+  color: #767676 !important;
+  margin: 0px !important;
+  word-wrap: break-word !important;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+  font-size: 14px !important;
+  line-height: 18px !important;
+  letter-spacing: normal !important;
+  padding-top: 0px !important;
+  padding-bottom: 0px !important;
+  font-weight: normal !important;
+  -webkit-font-smoothing: antialiased;
+  box-sixing: border-box;
+  display: block;
+`;
+const Div_qtix31 = Styled.div`
+  display: table !important;
+  box-sizing: border-box;
+`;
+const Div_ni9axhe = Styled.div`
+  display: table-cell !important;
+  vertical-align: middle !important;
+`;
+const Div_36rlri = Styled.table`
+  display: inline-block !important;
+  box-sizing: border-box;
+`;
+const Small_1wk8be92 = Styled.div`
+  margin: 0px !important;
+  word-wrap: break-word !important;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+  font-size: 12px !important;
+  line-height: 16px !important;
+  letter-spacing: normal !important;
+  padding-top: 0px !important;
+  padding-bottom: 0px !important;
+  font-weight: 800 !important;
+  color: #767676 !important;
+  -webkit-font-smoothing: antialiased;
+`;
+const Span_okqgdgp = Styled.span`
+  font-weight: 600 !important;
+  color: #484848 !important;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+  margin: 0px !important;
+  word-wrap: break-word !important;
+  font-size: 16px !important;
+  line-height: 22px !important;
+  letter-spacing: normal !important;
+  display: inline !important;
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+`;
+const Span_jz15f0c = Styled.span`
+  color: #484848 !important;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+  margin: 0px !important;
+  word-wrap: break-word !important;
+  font-size: 16px !important;
+  line-height: 22px !important;
+  letter-spacing: normal !important;
+  font-weight: normal !important;
+  display: inline !important;
+  -webkit-font-smoothing: antialiased;
+`;
+const Button_aijsh5 = Styled.button`
+  -webkit-appearance: none !important;
+  background: transparent !important;
+  border: 0px !important;
+  cursor: pointer !important;
+  margin: 0px !important;
+  padding: 0px !important;
+  text-align: left !important;
+  user-select: auto !important;
+  color: inherit !important;
+  vertical-align: bottom !important;
+  font-weight: inherit !important;
+  display: inline-block !important;
+  font-size: 14px !important;
+`;
+
+
+const Highlights = props => (
+  <div style={{ marginBottom: '24px' }}>
+    <Div_1d6n3ar>
+      <Small_1wk8be92>
+        <span>HOME HIGHLIGHTS</span>
+      </Small_1wk8be92>
+      <div style={{ marginTop: '12px' }}>
         <div>
-          <div style="margin-bottom:8px">
-            <span class="_okqgdgp">Hot tub<span aria-hidden="true"> · </span></span>
-            <span class="_jz15f0c">This is one of few homes in this area that has this feature.</span>
+          <div style={{ marginBottom: '8px' }}>
+            <Span_okqgdgp>Hot tub</Span_okqgdgp>
+            <Span_jz15f0c>
+              <span> · </span>
+              This is one of a few homes in this area that has this feature.
+            </Span_jz15f0c>
           </div>
-        <div class="_5y40cyp">
-      <div class="_10k87om">
-    <div>
-      <button type="button" class="_aijsh5" aria-busy="false">
-        <div class="_qtix31">
-          <div class="_ni9axhe">
-            <span>Helpful</span>
-          </div>
-            <div class="_ni9axhe">
-              <div style="margin-left:4px">
-                <svg viewBox="0 0 16 16" role="presentation" aria-hidden="true" focusable="false" style="height:1em;width:1em;display:block;fill:currentColor">
-                  <path d="m8.37 1c-.34 0-.53.43-.64.79l-.04.17c-.17.74-.56 2.47-3.76 3.58-1.97.68-2.93 2.31-2.93 4.96 0 1.68.56 4.5 4.3 4.5h6.2c.3 0 1-.06 1-.63 0-.41-.26-.63-.5-.63a.5.5 0 1 1 0-1c .92 0 1-.31 1-.63 0-.52-.38-.61-.54-.63a.5.5 0 0 1 -.46-.52.5.5 0 0 1 .5-.48c1 0 1-.41 1-.63s0-.62-1-.62a.5.5 0 1 1 0-1c1 0 1-.47 1-.63 0-.58-.77-.63-1-.63h-4.5a1 1 0 0 1 -.83-1.56c.05-.07.16-.19.31-.35.34-.35.92-.93 1.21-1.58.24-.52.35-1.25.28-1.78 0-.01-.12-.74-.62-.74m3.15 15.04h-6.2c-3.32 0-5.3-2.06-5.3-5.5 0-3.09 1.21-5.08 3.61-5.91 2.67-.93 2.97-2.23 3.11-2.86.02-.08.04-.16.05-.22.37-1.31 1.16-1.51 1.6-1.51 1.02 0 1.52.96 1.61 1.61.09.71-.06 1.64-.37 2.32-.37.81-1.02 1.47-1.41 1.86-.09.1-.17.17-.2.21h4.5c1.2 0 2 .65 2 1.63 0 .46-.16.84-.45 1.11.39.37.45.86.45 1.14 0 .34-.09.98-.74 1.35.15.23.24.53.24.9 0 .41-.12 1.01-.74 1.36.15.26.24.56.24.89 0 .97-.8 1.63-2 1.63">
-                  </path>
-                </svg>
+          <Div_5y40cyp>
+            <Div_10k87om>
+              <div>
+                <Button_aijsh5 aria-busy="false">
+                  <Div_qtix31>
+                    <Div_ni9axhe>
+                      <span>Helpful</span>
+                    </Div_ni9axhe>
+                    <Div_ni9axhe>
+                      <div style={{ marginLeft: '4px' }}>
+                        <svg viewBox="0 0 16 16" role="presentation" aria-hidden="true" focusable="false" style={{ height: '1em', width: '1em', display: 'block', fill: 'currentcolor' }}>
+                        <path d="m8.37 1c-.34 0-.53.43-.64.79l-.04.17c-.17.74-.56 2.47-3.76 3.58-1.97.68-2.93 2.31-2.93 4.96 0 1.68.56 4.5 4.3 4.5h6.2c.3 0 1-.06 1-.63 0-.41-.26-.63-.5-.63a.5.5 0 1 1 0-1c .92 0 1-.31 1-.63 0-.52-.38-.61-.54-.63a.5.5 0 0 1 -.46-.52.5.5 0 0 1 .5-.48c1 0 1-.41 1-.63s0-.62-1-.62a.5.5 0 1 1 0-1c1 0 1-.47 1-.63 0-.58-.77-.63-1-.63h-4.5a1 1 0 0 1 -.83-1.56c.05-.07.16-.19.31-.35.34-.35.92-.93 1.21-1.58.24-.52.35-1.25.28-1.78 0-.01-.12-.74-.62-.74m3.15 15.04h-6.2c-3.32 0-5.3-2.06-5.3-5.5 0-3.09 1.21-5.08 3.61-5.91 2.67-.93 2.97-2.23 3.11-2.86.02-.08.04-.16.05-.22.37-1.31 1.16-1.51 1.6-1.51 1.02 0 1.52.96 1.61 1.61.09.71-.06 1.64-.37 2.32-.37.81-1.02 1.47-1.41 1.86-.09.1-.17.17-.2.21h4.5c1.2 0 2 .65 2 1.63 0 .46-.16.84-.45 1.11.39.37.45.86.45 1.14 0 .34-.09.98-.74 1.35.15.23.24.53.24.9 0 .41-.12 1.01-.74 1.36.15.26.24.56.24.89 0 .97-.8 1.63-2 1.63">
+                        </path>
+                        </svg>
+                      </div>
+                    </Div_ni9axhe>
+                  </Div_qtix31>
+                </Button_aijsh5>
+                <Div_36rlri style={{ marginLeft: '4px', marginRight: '4px' }}>
+                  <span aria-hidden="true"> · </span>
+                </Div_36rlri>
+                <Button_aijsh5>
+                  <span>Not helpful</span>
+                </Button_aijsh5>
               </div>
-            </div>
-          </div>
-        </button>
-          <div class="_36rlri" style="margin-left:4px;margin-right:4px">
-            <span aria-hidden="true"> · </span>
-          </div>
-            <button type="button" class="_aijsh5" aria-busy="false">
-              <span>Not helpful</span>
-            </button>
-          </div>
+            </Div_10k87om>
+          </Div_5y40cyp>
         </div>
       </div>
-    </div>
-  </div>
-    <div style="margin-top:12px">
-      <div>
-        <div style="margin-bottom:8px">
-          <span class="_okqgdgp">Laskarina is a Superhost<span aria-hidden="true"> · </span></span>
-          <span class="_jz15f0c">Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</span>
-        </div>
-          <div class="_5y40cyp">
-            <div class="_10k87om">
+      {/* end of first home highlight 1 of 3 */}
+      <div style={{ marginTop: '12px' }}>
+        <div>
+          <div style={{ marginBottom: '8px' }}>
+            <Span_okqgdgp>Sparkling clean</Span_okqgdgp>
+            <Span_jz15f0c>
+              <span> · </span>
+              5 recent guests have said that this home was sparkling clean.
+            </Span_jz15f0c>
+          </div>
+          <Div_5y40cyp>
+            <Div_10k87om>
               <div>
-                <button type="button" class="_aijsh5" aria-busy="false">
-                  <div class="_qtix31">
-                    <div class="_ni9axhe">
+                <Button_aijsh5 aria-busy="false">
+                  <Div_qtix31>
+                    <Div_ni9axhe>
                       <span>Helpful</span>
-                    </div>
-                      <div class="_ni9axhe">
-                        <div style="margin-left:4px">
-                          <svg viewBox="0 0 16 16" role="presentation" aria-hidden="true" focusable="false" style="height:1em;width:1em;display:block;fill:currentColor">
-                          <path d="m8.37 1c-.34 0-.53.43-.64.79l-.04.17c-.17.74-.56 2.47-3.76 3.58-1.97.68-2.93 2.31-2.93 4.96 0 1.68.56 4.5 4.3 4.5h6.2c.3 0 1-.06 1-.63 0-.41-.26-.63-.5-.63a.5.5 0 1 1 0-1c .92 0 1-.31 1-.63 0-.52-.38-.61-.54-.63a.5.5 0 0 1 -.46-.52.5.5 0 0 1 .5-.48c1 0 1-.41 1-.63s0-.62-1-.62a.5.5 0 1 1 0-1c1 0 1-.47 1-.63 0-.58-.77-.63-1-.63h-4.5a1 1 0 0 1 -.83-1.56c.05-.07.16-.19.31-.35.34-.35.92-.93 1.21-1.58.24-.52.35-1.25.28-1.78 0-.01-.12-.74-.62-.74m3.15 15.04h-6.2c-3.32 0-5.3-2.06-5.3-5.5 0-3.09 1.21-5.08 3.61-5.91 2.67-.93 2.97-2.23 3.11-2.86.02-.08.04-.16.05-.22.37-1.31 1.16-1.51 1.6-1.51 1.02 0 1.52.96 1.61 1.61.09.71-.06 1.64-.37 2.32-.37.81-1.02 1.47-1.41 1.86-.09.1-.17.17-.2.21h4.5c1.2 0 2 .65 2 1.63 0 .46-.16.84-.45 1.11.39.37.45.86.45 1.14 0 .34-.09.98-.74 1.35.15.23.24.53.24.9 0 .41-.12 1.01-.74 1.36.15.26.24.56.24.89 0 .97-.8 1.63-2 1.63"></path>
-                          </svg>
-                        </div>
+                    </Div_ni9axhe>
+                    <Div_ni9axhe>
+                      <div style={{ marginLeft: '4px' }}>
+                        <svg viewBox="0 0 16 16" role="presentation" aria-hidden="true" focusable="false" style={{ height: '1em', width: '1em', display: 'block', fill: 'currentcolor' }}>
+                        <path d="m8.37 1c-.34 0-.53.43-.64.79l-.04.17c-.17.74-.56 2.47-3.76 3.58-1.97.68-2.93 2.31-2.93 4.96 0 1.68.56 4.5 4.3 4.5h6.2c.3 0 1-.06 1-.63 0-.41-.26-.63-.5-.63a.5.5 0 1 1 0-1c .92 0 1-.31 1-.63 0-.52-.38-.61-.54-.63a.5.5 0 0 1 -.46-.52.5.5 0 0 1 .5-.48c1 0 1-.41 1-.63s0-.62-1-.62a.5.5 0 1 1 0-1c1 0 1-.47 1-.63 0-.58-.77-.63-1-.63h-4.5a1 1 0 0 1 -.83-1.56c.05-.07.16-.19.31-.35.34-.35.92-.93 1.21-1.58.24-.52.35-1.25.28-1.78 0-.01-.12-.74-.62-.74m3.15 15.04h-6.2c-3.32 0-5.3-2.06-5.3-5.5 0-3.09 1.21-5.08 3.61-5.91 2.67-.93 2.97-2.23 3.11-2.86.02-.08.04-.16.05-.22.37-1.31 1.16-1.51 1.6-1.51 1.02 0 1.52.96 1.61 1.61.09.71-.06 1.64-.37 2.32-.37.81-1.02 1.47-1.41 1.86-.09.1-.17.17-.2.21h4.5c1.2 0 2 .65 2 1.63 0 .46-.16.84-.45 1.11.39.37.45.86.45 1.14 0 .34-.09.98-.74 1.35.15.23.24.53.24.9 0 .41-.12 1.01-.74 1.36.15.26.24.56.24.89 0 .97-.8 1.63-2 1.63">
+                        </path>
+                        </svg>
                       </div>
-                    </div>
-                </button>
-                  <div class="_36rlri" style="margin-left:4px;margin-right:4px">
-                    <span aria-hidden="true"> · </span>
-                  </div>
-                    <button type="button" class="_aijsh5" aria-busy="false">
-                      <span>Not helpful</span>
-                    </button>
-                  </div>
-                </div>
+                    </Div_ni9axhe>
+                  </Div_qtix31>
+                </Button_aijsh5>
+                <Div_36rlri style={{ marginLeft: '4px', marginRight: '4px' }}>
+                  <span aria-hidden="true"> · </span>
+                </Div_36rlri>
+                <Button_aijsh5>
+                  <span>Not helpful</span>
+                </Button_aijsh5>
               </div>
-            </div>
+            </Div_10k87om>
+          </Div_5y40cyp>
+        </div>
+      </div>
+      {/* end of the second home highlight */}
+      <div style={{ marginTop: '12px' }}>
+        <div>
+          <div style={{ marginBottom: '8px' }}>
+            <Span_okqgdgp>Great location</Span_okqgdgp>
+            <Span_jz15f0c>
+              <span> · </span>
+              100% of recent guests gave this home's location a 5-star rating.
+            </Span_jz15f0c>
           </div>
-            <div style="margin-top:12px">
+          <Div_5y40cyp>
+            <Div_10k87om>
               <div>
-                <div style="margin-bottom:8px">
-                  <span class="_okqgdgp">Great check-in experience<span aria-hidden="true"> · </span></span>
-                  <span class="_jz15f0c">100% of recent guests gave this home’s check-in process a 5-star rating.</span>
-                </div>
-                  <div class="_5y40cyp">
-                    <div class="_10k87om">
-                      <div>
-                        <button type="button" class="_aijsh5" aria-busy="false">
-                          <div class="_qtix31">
-                            <div class="_ni9axhe">
-                              <span>Helpful</span>
-                            </div>
-                              <div class="_ni9axhe">
-                                <div style="margin-left:4px">
-                                  <svg viewBox="0 0 16 16" role="presentation" aria-hidden="true" focusable="false" style="height:1em;width:1em;display:block;fill:currentColor">
-                                  <path d="m8.37 1c-.34 0-.53.43-.64.79l-.04.17c-.17.74-.56 2.47-3.76 3.58-1.97.68-2.93 2.31-2.93 4.96 0 1.68.56 4.5 4.3 4.5h6.2c.3 0 1-.06 1-.63 0-.41-.26-.63-.5-.63a.5.5 0 1 1 0-1c .92 0 1-.31 1-.63 0-.52-.38-.61-.54-.63a.5.5 0 0 1 -.46-.52.5.5 0 0 1 .5-.48c1 0 1-.41 1-.63s0-.62-1-.62a.5.5 0 1 1 0-1c1 0 1-.47 1-.63 0-.58-.77-.63-1-.63h-4.5a1 1 0 0 1 -.83-1.56c.05-.07.16-.19.31-.35.34-.35.92-.93 1.21-1.58.24-.52.35-1.25.28-1.78 0-.01-.12-.74-.62-.74m3.15 15.04h-6.2c-3.32 0-5.3-2.06-5.3-5.5 0-3.09 1.21-5.08 3.61-5.91 2.67-.93 2.97-2.23 3.11-2.86.02-.08.04-.16.05-.22.37-1.31 1.16-1.51 1.6-1.51 1.02 0 1.52.96 1.61 1.61.09.71-.06 1.64-.37 2.32-.37.81-1.02 1.47-1.41 1.86-.09.1-.17.17-.2.21h4.5c1.2 0 2 .65 2 1.63 0 .46-.16.84-.45 1.11.39.37.45.86.45 1.14 0 .34-.09.98-.74 1.35.15.23.24.53.24.9 0 .41-.12 1.01-.74 1.36.15.26.24.56.24.89 0 .97-.8 1.63-2 1.63"></path>
-                                  </svg>
-                                </div>
-                              </div>
-                            </div>
-                          </button>
-                            <div class="_36rlri" style="margin-left:4px;margin-right:4px">
-                              <span aria-hidden="true"> · </span>
-                            </div>
-                              <button type="button" class="_aijsh5" aria-busy="false">
-                                <span>Not helpful</span>
-                              </button>
-                            </div>
-                          </div>
-                        </div>
+                <Button_aijsh5 aria-busy="false">
+                  <Div_qtix31>
+                    <Div_ni9axhe>
+                      <span>Helpful</span>
+                    </Div_ni9axhe>
+                    <Div_ni9axhe>
+                      <div style={{ marginLeft: '4px' }}>
+                        <svg viewBox="0 0 16 16" role="presentation" aria-hidden="true" focusable="false" style={{ height: '1em', width: '1em', display: 'block', fill: 'currentcolor' }}>
+                        <path d="m8.37 1c-.34 0-.53.43-.64.79l-.04.17c-.17.74-.56 2.47-3.76 3.58-1.97.68-2.93 2.31-2.93 4.96 0 1.68.56 4.5 4.3 4.5h6.2c.3 0 1-.06 1-.63 0-.41-.26-.63-.5-.63a.5.5 0 1 1 0-1c .92 0 1-.31 1-.63 0-.52-.38-.61-.54-.63a.5.5 0 0 1 -.46-.52.5.5 0 0 1 .5-.48c1 0 1-.41 1-.63s0-.62-1-.62a.5.5 0 1 1 0-1c1 0 1-.47 1-.63 0-.58-.77-.63-1-.63h-4.5a1 1 0 0 1 -.83-1.56c.05-.07.16-.19.31-.35.34-.35.92-.93 1.21-1.58.24-.52.35-1.25.28-1.78 0-.01-.12-.74-.62-.74m3.15 15.04h-6.2c-3.32 0-5.3-2.06-5.3-5.5 0-3.09 1.21-5.08 3.61-5.91 2.67-.93 2.97-2.23 3.11-2.86.02-.08.04-.16.05-.22.37-1.31 1.16-1.51 1.6-1.51 1.02 0 1.52.96 1.61 1.61.09.71-.06 1.64-.37 2.32-.37.81-1.02 1.47-1.41 1.86-.09.1-.17.17-.2.21h4.5c1.2 0 2 .65 2 1.63 0 .46-.16.84-.45 1.11.39.37.45.86.45 1.14 0 .34-.09.98-.74 1.35.15.23.24.53.24.9 0 .41-.12 1.01-.74 1.36.15.26.24.56.24.89 0 .97-.8 1.63-2 1.63">
+                        </path>
+                        </svg>
                       </div>
-                    </div>
-                  </div>
-                </div>
+                    </Div_ni9axhe>
+                  </Div_qtix31>
+                </Button_aijsh5>
+                <Div_36rlri style={{ marginLeft: '4px', marginRight: '4px' }}>
+                  <span aria-hidden="true"> · </span>
+                </Div_36rlri>
+                <Button_aijsh5>
+                  <span>Not helpful</span>
+                </Button_aijsh5>
               </div>
-            </div>
-          </div>
+            </Div_10k87om>
+          </Div_5y40cyp>
+        </div>
+      </div>
+    </Div_1d6n3ar>
+  </div>
 );
 
 export default Highlights;
