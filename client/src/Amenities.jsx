4879de7e9ea87fa9840bf13fa7e0b471b24166fc
Jonathan Pizzolato <jpizzolato36@gmail.com>
2018-06-08 14:09:15 -0700

    Basic shell coded out with placeholder svg's and text.

---
+++ client/src/Amenities.jsx
@@ -0,0 +1,205 @@
+import React from 'react';
+import Styled from 'styled-components';
+
+const Div_2h22gn = Styled.div`
+  margin-left: -8px !important;
+  margin-right: -8px !important;
+  box-sizing: border-box;
+`;
+const Div_iq8x9is = Styled.div`
+  padding-left: 8px !important;
+  padding-right: 8px !important;
+  min-height: 1px !important;
+  position: relative !important;
+  width: 50% !important;
+  float: left !important;
+  display: block;
+  box-sizing: border-box;
+`;
+const Div_qtix31 = Styled.div`
+  display: table !important;
+  box-sizing: border-box;
+`;
+const Div_1wt9k7hn = Styled.div`
+  color: #484848 !important;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+  margin: 0px !important;
+  word-wrap: break-word !important;
+  font-size: 16px !important;
+  line-height: 22px !important;
+  letter-spacing: normal !important;
+  font-weight: 800 !important;
+`;
+const Div_ni9axhe = Styled.div`
+  display: table-cell !important;
+  vertical-align: middle !important;
+`;
+const Div_1fcn46ls = Styled.div`
+  color: #484848 !important;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+  margin: 0px !important;
+  word-wrap: break-word !important;
+  font-size: 16px !important;
+  line-height: 22px !important;
+  letter-spacing: normal !important;
+  font-weight: normal !important;
+`;
+const Div_157yfd15 = Styled.div`
+  font-weight: 600 !important;
+  color: #484848 !important;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+  margin: 0px !important;
+  word-wrap: break-word !important;
+  font-size: 16px !important;
+  line-height: 22px !important;
+  letter-spacing: normal !important;
+`;
+const H2_1xu9tpch = Styled.h2`
+  color: inherit !important;
+  font-size: 1em !important;
+  font-weight: inherit !important;
+  line-height: inherit !important;
+  margin: 0px !important;
+  padding: 0px !important;
+  display: inline !important;
+`;
+const Button_b82bweu = Styled.button`
+  color: #008489 !important;
+  font: inherit !important;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+  text-decoration: none !important;
+  -webkit-appearance: none !important;
+  -webkit-font-smoothing: antialiased;
+  background: transparent !important;
+  border: 0px !important;
+  cursor: pointer !important;
+  margin: 0px !important;
+  padding: 0px !important;
+  user-select: auto !important;
+  text-align: left !important;
+`;
+
+const Amenities = props => (
+  <div style={{ marginBottom: '0px' }}>
+    <section>
+      <div style={{ marginBottom: '16px' }}>
+        <H2_1xu9tpch>
+          <Div_1wt9k7hn>
+            <span>Amenities</span>
+          </Div_1wt9k7hn>
+        </H2_1xu9tpch>
+      </div>
+      <Div_2h22gn>
+        <Div_iq8x9is>
+          <div style={{ marginBottom: '16px' }}>
+            <Div_qtix31>
+              <Div_ni9axhe>
+                <div style={{ marginRight: '8px' }}>
+                  <svg viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false" style={{ height: '1.2em', width: '1.2em', fill: 'currentcolor' }}>
+                  <path d="m10.5 0a .5.5 0 0 0 -.5.5v7a .5.5 0 0 1 -.49.5h-1.51v-7.5a.5.5 0 1 0 -1 0v7.5h-1.51a.5.5 0 0 1 -.49-.5v-7a .5.5 0 1 0 -1 0v7c0 .83.67 1.5 1.49 1.5h1.51v5c0 .03.01.06.02.09a1.49 1.49 0 0 0 -1.02 1.41v7c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5v-7c0-.66-.43-1.21-1.02-1.41.01-.03.02-.06.02-.09v-5h1.51a1.5 1.5 0 0 0 1.49-1.5v-7a .5.5 0 0 0 -.5-.5zm-2.5 15.5v7a .5.5 0 0 1 -.5.5.5.5 0 0 1 -.5-.5v-7c0-.28.22-.5.5-.5s.5.22.5.5zm11.5-15.5h-2c-1.4 0-2.5 1.07-2.5 2.5v7c0 1.43 1.1 2.5 2.5 2.5h1.5v2.09a1.49 1.49 0 0 0 -.5-.09c-.83 0-1.5.67-1.5 1.5v7c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5v-22.5zm-2 11c-.86 0-1.5-.63-1.5-1.5v-7c0-.87.65-1.5 1.5-1.5h1.5v10zm1.5 11.5a.5.5 0 0 1 -.5.5.5.5 0 0 1 -.5-.5v-7c0-.28.22-.5.5-.5s.5.22.5.5z" fill-rule="evenodd">
+                  </path>
+                  </svg>
+                </div>
+              </Div_ni9axhe>
+              <Div_ni9axhe>
+                <Div_1fcn46ls>Kitchen</Div_1fcn46ls>
+              </Div_ni9axhe>
+            </Div_qtix31>
+          </div>
+          {/* end of first amenity */}
+          <div style={{ marginBottom: '16px' }}>
+            <Div_qtix31>
+              <Div_ni9axhe>
+                <div style={{ marginRight: '8px' }}>
+                  <svg viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false" style={{ height: '1.2em', width: '1.2em', fill: 'currentcolor' }}>
+                  <path d="m12 15a3 3 0 1 0 0 6 3 3 0 0 0 0-6zm0 5a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm5.92-5.78a.5.5 0 1 1 -.84.55c-1.19-1.81-3.07-2.77-5.08-2.77s-3.89.96-5.08 2.78a.5.5 0 0 1 -.84-.55c1.38-2.1 3.58-3.23 5.92-3.23s4.54 1.13 5.92 3.23zm2.98-3.03a.5.5 0 1 1 -.79.61c-1.66-2.14-5.22-3.8-8.11-3.8-2.83 0-6.26 1.62-8.12 3.82a.5.5 0 0 1 -.76-.65c2.05-2.42 5.75-4.17 8.88-4.17 3.19 0 7.05 1.8 8.9 4.19zm2.95-2.33a.5.5 0 0 1 -.71-.02c-2.94-3.07-6.71-4.84-11.14-4.84s-8.2 1.77-11.14 4.85a.5.5 0 0 1 -.72-.69c3.12-3.27 7.14-5.16 11.86-5.16s8.74 1.89 11.86 5.16a.5.5 0 0 1 -.02.71z" fill-rule="evenodd">
+                  </path>
+                  </svg>
+                </div>
+              </Div_ni9axhe>
+              <Div_ni9axhe>
+                <Div_1fcn46ls>Wifi</Div_1fcn46ls>
+              </Div_ni9axhe>
+            </Div_qtix31>
+          </div>
+          {/* end of second amenity */}
+          <div style={{ marginBottom: '16px' }}>
+            <Div_qtix31>
+              <Div_ni9axhe>
+                <div style={{ marginRight: '8px' }}>
+                  <svg viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false" style={{ height: '1.2em', width: '1.2em', fill: 'currentcolor' }}>
+                  <path d="m23.5 23h-.5v-11.09a1.49 1.49 0 0 0 1-1.41 1.5 1.5 0 0 0 -1.5-1.5h-20.99a1.5 1.5 0 0 0 -.5 2.91v11.09h-.5a.5.5 0 1 0 0 1h23a .5.5 0 0 0 0-1zm-19.5 0h-2v-11h2zm3 0h-2v-11h2zm3 0h-2v-11h2zm3 0h-2v-11h2zm3 0h-2v-11h2zm3 0h-2v-11h2zm3 0h-2v-11h2zm0-12h-20.5a.5.5 0 0 1 -.5-.5c0-.28.22-.5.5-.5h20.99a.5.5 0 0 1 .5.5c0 .28-.22.5-.5.5zm-10.88-7.29c-.92-1.26-.76-2.38.06-3.18a2.6 2.6 0 0 1 .66-.48.5.5 0 1 1 .43.9 1.66 1.66 0 0 0 -.4.29c-.48.47-.56 1.04.01 1.83 2.09 2.27 2.09 4.26.34 4.89a.5.5 0 0 1 -.34-.94c.94-.34.94-1.46-.77-3.32zm4 0c-.92-1.26-.76-2.38.06-3.18a2.6 2.6 0 0 1 .66-.48.5.5 0 1 1 .43.9 1.66 1.66 0 0 0 -.4.29c-.48.47-.56 1.04.01 1.83 2.09 2.27 2.09 4.26.34 4.89a.5.5 0 0 1 -.34-.94c.94-.34.94-1.46-.77-3.32zm-8 0c-.92-1.26-.76-2.38.06-3.18a2.6 2.6 0 0 1 .66-.48.5.5 0 1 1 .43.9 1.66 1.66 0 0 0 -.4.29c-.48.47-.56 1.04.01 1.83 2.09 2.27 2.09 4.26.34 4.89a.5.5 0 0 1 -.34-.94c.94-.34.94-1.46-.77-3.32z" fill-rule="evenodd">
+                  </path>
+                  </svg>
+                </div>
+              </Div_ni9axhe>
+              <Div_ni9axhe>
+                <Div_1fcn46ls>Hot tub</Div_1fcn46ls>
+              </Div_ni9axhe>
+            </Div_qtix31>
+          </div>
+          {/* end of third amenity... move over column */}
+        </Div_iq8x9is>
+        <Div_iq8x9is>
+          <div style={{ marginBottom: '16px' }}>
+            <Div_qtix31>
+              <Div_ni9axhe>
+                <div style={{ marginRight: '8px' }}>
+                  <svg viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false" style={{ height: '1.2em', width: '1.2em', fill: 'currentcolor' }}>
+                  <path d="m5.5 5h-.5v8h14v-8zm12.5 7h-12v-6h12zm5.9 6.69-.04-.05-.11-.14-.4-.51-1.2-1.55-1.15-1.47v-10.97a1 1 0 0 0 -.99-1h-16.02c-.55 0-.99.45-.99 1v10.95l-.19.24c-.6.76-1.2 1.53-1.76 2.26l-.16.21c-.29.37-.55.72-.79 1.04l-.1.13v.17c0 1.35.65 2 2 2h20c1.17 0 2-.71 2-2v-.17l-.11-.14zm-19.9-14.69h16.01c-.01 0-.01 7.92-.01 10h-16zm18 16h-20c-.73 0-.96-.2-.99-.85.21-.28.43-.56.66-.87l.16-.21a229.93 229.93 0 0 1 2.33-2.98l.07-.09h15.51l1.6 2.06 1.2 1.55a4708182.18 4708182.18 0 0 1 .42.54c-.05.59-.38.86-.98.86zm-7.52-3.62a.5.5 0 0 0 -.48-.38h-4a .5.5 0 0 0 -.49.38l-.5 2a .5.5 0 0 0 .49.62h5a .5.5 0 0 0 .49-.62l-.5-2zm-4.34 1.62.25-1h3.22l.25 1z" fill-rule="evenodd">
+                  </path>
+                  </svg>
+                </div>
+              </Div_ni9axhe>
+              <Div_ni9axhe>
+                <Div_1fcn46ls>Laptop friendly workspace</Div_1fcn46ls>
+              </Div_ni9axhe>
+            </Div_qtix31>
+          </div>
+          {/* end of fourth amenity */}
+          <div style={{ marginBottom: '16px' }}>
+            <Div_qtix31>
+              <Div_ni9axhe>
+                <div style={{ marginRight: '8px' }}>
+                  <svg viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false" style={{ height: '1.2em', width: '1.2em', fill: 'currentcolor' }}>
+                  <path d="m23.5 4h-1.5v-1.5c0-1.38-1.12-2.5-2.5-2.5h-15a2.5 2.5 0 0 0 -2.5 2.5v1.5h-1.5a.5.5 0 1 0 0 1h1.5v15.5c0 .83.67 1.5 1.5 1.5h1.5v.5c0 .83.67 1.5 1.5 1.5h14c .83 0 1.5-.67 1.5-1.5v-17.5h1.5a.5.5 0 0 0 0-1zm-20.5-1.5c0-.82.67-1.5 1.5-1.5h15.01c.82 0 1.49.67 1.49 1.5v1.5h-2v-.51a.5.5 0 0 1 .5-.49.5.5 0 0 0 0-1 1.5 1.5 0 0 0 -1.5 1.5v11.5h-15zm0 13.5h15v2h-15zm .5 5a .5.5 0 0 1 -.5-.5v-1.5h15v1.51c0 .27-.23.5-.5.5h-14zm17.5 1.5c0 .28-.22.5-.5.5h-14a .5.5 0 0 1 -.5-.5v-.5h11.5c.83 0 1.5-.67 1.5-1.5v-15.5h2z" fill-rule="evenodd">
+                  </path>
+                  </svg>
+                </div>
+              </Div_ni9axhe>
+              <Div_ni9axhe>
+                <Div_1fcn46ls>Essentials</Div_1fcn46ls>
+              </Div_ni9axhe>
+            </Div_qtix31>
+          </div>
+          {/* end of fifth amenity */}
+          <div style={{ marginBottom: '16px' }}>
+            <Div_qtix31>
+              <Div_ni9axhe>
+                <div style={{ marginRight: '8px' }}>
+                  <svg viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false" style={{ height: '1.2em', width: '1.2em', fill: 'currentcolor' }}>
+                  <path d="m23.5 23h-1.5v-19a2 2 0 0 0 -.01-4h-19.98a2 2 0 0 0 -.01 4v19h-1.5a.5.5 0 1 0 0 1h23a .5.5 0 0 0 0-1zm-19.5 0h-1v-19h1zm15-19v14h-1.17c.33-1.58.04-3.44-.59-4.72-.24-.5-.49-.93-.92-1.58 0 0-.26-.39-.33-.5-.92-1.43-1.25-2.32-1.16-3.66a.5.5 0 0 0 -.86-.38c-.17.17-.92.88-1.03 1-.8.79-1.29 1.44-1.54 2.19a3.84 3.84 0 0 0 -.23 1.12c-.03.49.01.95.1 1.64l.02.14c-.61-.35-.95-.93-1.14-1.76-.08-.34-.1-.48-.19-1.25a14.33 14.33 0 0 0 -.04-.3.5.5 0 0 0 -.79-.33c-1.8 1.31-3.19 4.37-3.17 7.04 0 .49.07.94.16 1.37h-1.12v-14.02zm-6.72 8.96c-.09-.63-.12-1.03-.09-1.44a2.88 2.88 0 0 1 .17-.85c.2-.58.61-1.13 1.3-1.82l.22-.21c.13 1.02.54 1.91 1.29 3.08l.33.51c.41.62.64 1.02.86 1.47.58 1.18.84 3.01.44 4.4-.52 1.82-2.05 2.89-4.89 2.89-3.31 0-4.89-1.66-4.91-4.38-.02-2 .89-4.28 2.09-5.6.04.28.07.44.13.68.33 1.44 1.1 2.4 2.58 2.78a.5.5 0 0 0 .62-.49c0-.24 0-.26-.11-1.04zm6.72 10.04h-14v-4h1.45c.8 1.87 2.62 3 5.44 3 2.96 0 4.82-1.11 5.63-3h1.48zm2 0h-1v-19h1zm0-20h-18.99a1 1 0 1 1 0-2h19.98a1 1 0 1 1 0 2z" fill-rule="evenodd">
+                  </path>
+                  </svg>
+                </div>
+              </Div_ni9axhe>
+              <Div_ni9axhe>
+                <Div_1fcn46ls>Essentials</Div_1fcn46ls>
+              </Div_ni9axhe>
+            </Div_qtix31>
+          </div>
+        </Div_iq8x9is>
+        {/* end of sixth amenity */}
+      </Div_2h22gn>
+      <div style={{ marginTop: '8px' }}>
+        <Div_157yfd15>
+          <Button_b82bweu>
+            <span>Show all 30 amenities</span>
+          </Button_b82bweu>
+        </Div_157yfd15>
+      </div>
+    </section>
+  </div>
+);
+
+export default Amenities;
